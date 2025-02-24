883a5e1b3b6e9be8718581203dc89c046dd6fb6a
Yavuz Alp Beydogan <yavuz.beydogan@hotmail.com>
2025-02-24 18:30:08 +0300

    düzenleme1

--- routers/auth.py
+++ routers/auth.py
@@ -90,3 +90,4 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
     token=create_access_token(user.username, user.id, user.role, timedelta(minutes=60))
     return{"access_token": token, "token_type":"bearer"}
+print("saf")
