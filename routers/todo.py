7f918aefdb3322c1472f80ccdf6c9259160239e9
Yavuz Alp Beydogan <yavuz.beydogan@hotmail.com>
2025-02-24 19:45:23 +0300

    Initial commit

--- routers/todo.py
+++ routers/todo.py
@@ -34,24 +34,28 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 async def read_all(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,)
-    return db.query(Todo).all()
+    return db.query(Todo).filter(Todo.owner_id==user.get('id')).all()
 
-@router.get("/get_by_id/{todo_id}", status_code=status.HTTP_200_OK)
-async def read_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
-    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+@router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_by_id(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,)
+    todo = db.query(Todo).filter(Todo.id == todo_id).filter(Todo.owner_id==user.get('id')).first()
     if todo is not None:
         return todo
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
 
 
-@router.post("/create_todo", status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
-    todo = Todo(**todo_request.dict())
+@router.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(user:user_dependency, db: db_dependency, todo_request: TodoRequest):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,)
+    todo = Todo(**todo_request.dict(), owner_id=user.get('id'))
     db.add(todo)
     db.commit()
 
 
-@router.put("/update_todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
     todo = db.query(Todo).filter(Todo.id == todo_id).first()
     if todo is None:
@@ -65,7 +69,7 @@ async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int
     db.commit()
 
 
-@router.delete("/delete_todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     todo = db.query(Todo).filter(Todo.id == todo_id).first()
     if todo is None:
